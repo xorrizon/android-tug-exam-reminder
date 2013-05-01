7dcb480acb2cecbd49a9f4fddd30ea4c1f041d19
Thomas Rosmarin <thomas.rosmarin@gmail.com>
2013-05-01 15:33:12 +0200

    add ExamReminderApplication

---
+++ exam-reminder/src/at/tugraz/examreminder/ExamReminderApplication.java
@@ -0,0 +1,11 @@
+package at.tugraz.examreminder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thoros
+ * Date: 01/05/13
+ * Time: 14:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExamReminderApplication {
+}
