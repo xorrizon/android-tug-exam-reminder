9916d79c4dbe3ca7a48eec86dcb624b3ba642605
Thomas Rosmarin <thomas.rosmarin@gmail.com>
2013-05-22 19:09:53 +0200

    removed warnings in crawler

--- exam-reminder/src/at/tugraz/examreminder/crawler/TuGrazSearchCrawler.java
+++ exam-reminder/src/at/tugraz/examreminder/crawler/TuGrazSearchCrawler.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import android.util.Log;
@@ -21,166 +23,178 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 public class TuGrazSearchCrawler implements Crawler {
 
-		final static String searchmachineUrl = "http://search.tugraz.at/search";
-		HashMap<String, String> attributeList = new HashMap<String, String>() {{
-			put("q", "analysis"); // Searchstring
-			put("site", "Alle");
-			put("btnG", "Suchen");
-			put("client", "tug_portal");
-			put("output", "xml_no_dtd");
-			put("sort", "date%3AD%3AL%3Ad1");
-			put("entqr", "3");
-			put("entqrm", "0");
-			put("entsp", "a");
-			put("oe", "UTF-8");
-			put("ie", "UTF-8");
-			put("ud", "1");
-			put("filter", "1");
-            put("hl", "de"); // Language
-        }};
+    final static String SEARCH_MACHINE_URI = "http://search.tugraz.at/search";
+    HashMap<String, String> SEARCH_MACHINE_URLI_ATTRIBUTES = new HashMap<String, String>() {{
+        put("q", ""); // Searchstring
+        put("site", "Alle");
+        put("btnG", "Suchen");
+        put("client", "tug_portal");
+        put("output", "xml_no_dtd");
+        put("sort", "date%3AD%3AL%3Ad1");
+        put("entqr", "3");
+        put("entqrm", "0");
+        put("entsp", "a");
+        put("oe", "UTF-8");
+        put("ie", "UTF-8");
+        put("ud", "1");
+        put("filter", "1");
+        put("hl", "de"); // Language
+    }};
 
-    final static String fieldStartTagCourseName = "<Field name=\"courseName\">";
-    final static String fieldStartTagCourseCode = "<Field name=\"courseCode\">";
-    final static String fieldEndTag = "</Field>";
+    final static SimpleDateFormat SEARCH_MACHINE_RESULTS_DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy HH:mm");
 
-    final static String tempFilename = "examreminder.tmp";
+    final static String tempSearchDataXmlFilename = "examreminder.tmp";
 
-	public String generateSearchUrl(String SearchTerm) {
-		String urlstring = searchmachineUrl;
-		//Set entrySet= attributeList.entrySet();
-		boolean isFirstAttribute = true;
-		for (Map.Entry<String, String> entry : attributeList.entrySet()) {
-			if(isFirstAttribute) {
-				urlstring += "?" + entry.getKey() + "=" + entry.getValue();
-				isFirstAttribute = false;
-			}
-			urlstring +=  "&" + entry.getKey() + "=" + entry.getValue();
-		}
-		return urlstring;
-	}
 
-    public void getResponseAndWriteToFile(String searchUrl) {
-        String uri = generateSearchUrl(searchUrl);
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpResponse response;
-        String responseString = null;
+    public String generateSearchUrl(String searchTerm) {
+        String searchUrl = "";
+        searchUrl += SEARCH_MACHINE_URI;
+        SEARCH_MACHINE_URLI_ATTRIBUTES.remove("q");
+        SEARCH_MACHINE_URLI_ATTRIBUTES.put("q", searchTerm);
+
+        boolean isFirstAttribute = true;
+        for (Map.Entry<String, String> entry : SEARCH_MACHINE_URLI_ATTRIBUTES.entrySet()) {
+            if (isFirstAttribute) {
+                searchUrl += "?" + entry.getKey() + "=" + entry.getValue();
+                isFirstAttribute = false;
+            }
+            searchUrl += "&" + entry.getKey() + "=" + entry.getValue();
+        }
+        return searchUrl;
+    }
+
+    public void getResponseXmlAndWriteToFile(String searchTerm) {
+        String searchUrl = generateSearchUrl(searchTerm);
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpResponse httpResponse;
         try {
-            response = httpclient.execute(new HttpGet(uri));
-            StatusLine statusLine = response.getStatusLine();
-            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-                File file = new File(ExamReminderApplication.getAppContext().getExternalFilesDir(null), tempFilename);
-                FileOutputStream out = new FileOutputStream(file);
-                response.getEntity().writeTo(out);
-                out.close();
-            } else{
-                //Closes the connection.
-                response.getEntity().getContent().close();
+            httpResponse = httpClient.execute(new HttpGet(searchUrl));
+            StatusLine statusLine = httpResponse.getStatusLine();
+            if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                File file = new File(ExamReminderApplication.getAppContext().getExternalFilesDir(null), tempSearchDataXmlFilename);
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                httpResponse.getEntity().writeTo(fileOutputStream);
+                fileOutputStream.close();
+            } else {
+                httpResponse.getEntity().getContent().close();
                 throw new IOException(statusLine.getReasonPhrase());
             }
         } catch (ClientProtocolException e) {
-            Log.v("SPAM", e.toString());
+            Log.v("TuGrazSearchCrawler", e.toString());
         } catch (IOException e) {
-            Log.v("SPAM", e.toString());
+            Log.v("TuGrazSearchCrawler", e.toString());
         }
-        return;
     }
 
-	@Override
-	public List<Course> getCourseList(String searchTerm) {
+    @Override
+    public List<Course> getCourseList(String searchTerm) {
         List<Course> foundCourse = new ArrayList<Course>();
-        getResponseAndWriteToFile("");
-        Map<String, String> coursemap = new HashMap<String, String>();
+        getResponseXmlAndWriteToFile("");
         Map<String, String> currentModuleMap = new HashMap<String, String>();
-        File tempfile = new File(ExamReminderApplication.getAppContext().getExternalFilesDir(null), tempFilename);
+        File tempfile = new File(ExamReminderApplication.getAppContext().getExternalFilesDir(null), tempSearchDataXmlFilename);
         Scanner scanner = null;
+
         try {
             scanner = new Scanner(tempfile);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            Log.v("TuGrazSearchCrawler", e.toString());
         }
-        int i = 0;
-        String currentTagValue = "";
-        String currentTagAttribute = "";
-        String line;
-        Course currentCourse = null;
+
+        String currentTagValue;
+        String currentTagAttribute;
+        String currentLine;
+        Course currentCourse;
+
+        assert scanner != null;
         while (scanner.hasNextLine()) {
-            line = scanner.nextLine();
-            if(line.contains("<MODULE_RESULT>")) {
+            currentLine = scanner.nextLine();
+            if (currentLine.contains("<MODULE_RESULT>")) {
                 currentModuleMap.clear();
-                while(scanner.hasNextLine()) {
-                    line = scanner.nextLine();
-                    if(line.contains("</MODULE_RESULT>")) {
-                        if(currentModuleMap.containsKey("WEB SERVICE") && (currentModuleMap.get("WEB SERVICE").toString().equals("CBO"))) {
+                while (scanner.hasNextLine()) {
+                    currentLine = scanner.nextLine();
+                    if (currentLine.contains("</MODULE_RESULT>")) {
+                        if (currentModuleMap.containsKey("WEB SERVICE") && (currentModuleMap.get("WEB SERVICE").toString().equals("CBO"))) {
                             currentCourse = new Course();
                             currentCourse.name = currentModuleMap.get("courseName");
                             currentCourse.number = currentModuleMap.get("courseCode");
                             currentCourse.term = currentModuleMap.get("teachingTerm");
                             currentCourse.type = currentModuleMap.get("teachingActivityID");
-                            if(currentModuleMap.containsKey("persons_name")) {
+                            if (currentModuleMap.containsKey("persons_name")) {
                                 currentCourse.lecturer = currentModuleMap.get("persons_name");
-                            }
-                            else if(currentModuleMap.containsKey("persons_name1")) {
+                            } else if (currentModuleMap.containsKey("persons_name1")) {
                                 currentCourse.lecturer = currentModuleMap.get("persons_name1");
                             }
-                            Log.v("TuGrazSearchCrawler", foundCourse.size()+" courses added");
 
                             foundCourse.add(currentCourse);
 
                         }
                         currentModuleMap.clear();
                     }
-                    if(line.contains("<Field") && line.contains("</Field>")) {
-                        currentTagAttribute = line.substring(line.indexOf("=\"")+ 2, line.indexOf("\">"));
-                        currentTagValue = line.substring(line.indexOf("\">")+ 2, line.indexOf("</Field>"));
+                    if (currentLine.contains("<Field") && currentLine.contains("</Field>")) {
+                        currentTagAttribute = currentLine.substring(currentLine.indexOf("=\"") + 2, currentLine.indexOf("\">"));
+                        currentTagValue = currentLine.substring(currentLine.indexOf("\">") + 2, currentLine.indexOf("</Field>"));
                         currentModuleMap.put(currentTagAttribute, currentTagValue);
 
-                    }
-                    else if(line.contains("<Field>")) {
+                    } else if (currentLine.contains("<Field>")) {
                         // TODO: for very very long descriptions;
                     }
-               }
+                }
             }
         }
 
         return foundCourse;
-	}
+    }
 
-	@Override
-	public SortedSet<Exam> getExams(Course course) {
+    @Override
+    public SortedSet<Exam> getExams(Course course) {
         SortedSet<Exam> foundExams = new TreeSet<Exam>();
-        getResponseAndWriteToFile("");
-        Map<String, String> coursemap = new HashMap<String, String>();
+        getResponseXmlAndWriteToFile("");
         Map<String, String> currentModuleMap = new HashMap<String, String>();
-        File tempfile = new File(ExamReminderApplication.getAppContext().getExternalFilesDir(null), tempFilename);
+        File tempfile = new File(ExamReminderApplication.getAppContext().getExternalFilesDir(null), tempSearchDataXmlFilename);
         Scanner scanner = null;
+
         try {
             scanner = new Scanner(tempfile);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            Log.v("TuGrazSearchCrawler", e.toString());
         }
-        int i = 0;
-        String currentTagValue = "";
-        String currentTagAttribute = "";
-        String line;
-        Exam currentExam = null;
+
+        String currentTagValue;
+        String currentTagAttribute;
+        String currentLine;
+        Exam currentExam;
+
         while (scanner.hasNextLine()) {
-            line = scanner.nextLine();
-            if(line.contains("<MODULE_RESULT>")) {
+            currentLine = scanner.nextLine();
+            if (currentLine.contains("<MODULE_RESULT>")) {
                 currentModuleMap.clear();
-                while(scanner.hasNextLine()) {
-                    line = scanner.nextLine();
-                    if(line.contains("</MODULE_RESULT>")) {
-                        if(currentModuleMap.containsKey("WEB SERVICE") && (currentModuleMap.get("WEB SERVICE").toString().equals("EBO"))) {
+                while (scanner.hasNextLine()) {
+                    currentLine = scanner.nextLine();
+                    if (currentLine.contains("</MODULE_RESULT>")) {
+                        if (currentModuleMap.containsKey("WEB SERVICE") && (currentModuleMap.get("WEB SERVICE").toString().equals("EBO"))) {
                             currentExam = new Exam();
 
-                            //currentExam.from = new Date(currentModuleMap.get("examStart"));
-                            //currentExam.to = new Date(currentModuleMap.get("examEnd"));
+                            try {
+                                currentExam.from = SEARCH_MACHINE_RESULTS_DATE_FORMAT.parse(currentModuleMap.get("examStart"));
+                            } catch (ParseException e) {
+                                currentExam.from = null;
+                            }
+
+                            try {
+                                currentExam.to = SEARCH_MACHINE_RESULTS_DATE_FORMAT.parse(currentModuleMap.get("examEnd"));
+                            } catch (ParseException e) {
+                                currentExam.to = null;
+                            }
                             currentExam.place = currentModuleMap.get("examLocation");
                             currentExam.term = currentModuleMap.get("teachingTerm");
                             currentExam.lecturer = currentModuleMap.get("lecturer");
                             currentExam.examinar = currentModuleMap.get("examinerName");
                             currentExam.registrationStart = null;
-                            //currentExam.registrationEnd = new Date(currentModuleMap.get("examStart"));
+                            try {
+                                currentExam.to = SEARCH_MACHINE_RESULTS_DATE_FORMAT.parse(currentModuleMap.get("examStart"));
+                            } catch (ParseException e) {
+                                currentExam.to = null;
+                            }
                             currentExam.participants = Integer.parseInt(currentModuleMap.get("numberOfParticipants"));
                             currentExam.participants_max = Integer.parseInt(currentModuleMap.get("maximumNumberOfParticipants"));
                             currentExam.updated_at = null;
@@ -189,26 +203,18 @@ public class TuGrazSearchCrawler implements Crawler {
                         }
                         currentModuleMap.clear();
                     }
-                    if(line.contains("<Field") && line.contains("</Field>")) {
-                        currentTagAttribute = line.substring(line.indexOf("=\"")+ 2, line.indexOf("\">"));
-                        currentTagValue = line.substring(line.indexOf("\">")+ 2, line.indexOf("</Field>"));
+                    if (currentLine.contains("<Field") && currentLine.contains("</Field>")) {
+                        currentTagAttribute = currentLine.substring(currentLine.indexOf("=\"") + 2, currentLine.indexOf("\">"));
+                        currentTagValue = currentLine.substring(currentLine.indexOf("\">") + 2, currentLine.indexOf("</Field>"));
                         currentModuleMap.put(currentTagAttribute, currentTagValue);
 
-                    }
-                    else if(line.contains("<Field>")) {
+                    } else if (currentLine.contains("<Field>")) {
                         // TODO: for very very long descriptions;
                     }
                 }
             }
         }
 
-        for(Exam exam : foundExams) {
-            Log.v("SPAM", "=================");
-            Log.v("SPAM", "exam.lecturer = " + exam.lecturer);
-            Log.v("SPAM", "exam.from = " + exam.from);
-        }
         return foundExams;
-	}
-
-
+    }
 }
