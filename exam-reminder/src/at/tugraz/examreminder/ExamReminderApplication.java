0bd77443c8f9109c6e14aaf9695bfd985e311470
Thomas Rosmarin <thomas.rosmarin@gmail.com>
2013-05-22 21:02:30 +0200

    add Application class for storage access

--- exam-reminder/src/at/tugraz/examreminder/ExamReminderApplication.java
+++ exam-reminder/src/at/tugraz/examreminder/ExamReminderApplication.java
@@ -1,11 +1,16 @@
 package at.tugraz.examreminder;
 
-/**
- * Created with IntelliJ IDEA.
- * User: thoros
- * Date: 01/05/13
- * Time: 14:25
- * To change this template use File | Settings | File Templates.
- */
-public class ExamReminderApplication {
+import android.app.Application;
+import android.content.Context;
+
+public class ExamReminderApplication extends Application{
+
+	private static Context context;
+    public void onCreate(){
+        context=getApplicationContext();
+    }
+
+    public static Context getAppContext(){
+        return context;
+    }
 }
