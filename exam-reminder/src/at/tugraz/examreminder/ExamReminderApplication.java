409e4eec399266458bdd9b6bead351a60f7c0aa9
Thomas Rosmarin <thomas.rosmarin@gmail.com>
2013-05-25 20:38:32 +0200

    add application class to access context in crawler

--- exam-reminder/src/at/tugraz/examreminder/ExamReminderApplication.java
+++ exam-reminder/src/at/tugraz/examreminder/ExamReminderApplication.java
@@ -2,15 +2,20 @@ package at.tugraz.examreminder;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
 public class ExamReminderApplication extends Application{
 
 	private static Context context;
+    @Override
     public void onCreate(){
+        super.onCreate();
         context=getApplicationContext();
+        Log.v("EXAM_REMINDER_APPLICATION", "ONCREATED");
     }
 
     public static Context getAppContext(){
+        Log.v("EXAM_REMINDER_APPLICATION", "GIVE ME THE SWEET CONTEXT");
         return context;
     }
 }
