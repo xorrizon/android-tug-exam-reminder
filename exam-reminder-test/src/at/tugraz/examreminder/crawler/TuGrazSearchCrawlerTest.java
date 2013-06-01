26274884676755ae700c62b8ebba9c8d06084993
Thomas Rosmarin <thomas.rosmarin@gmail.com>
2013-06-01 12:46:51 +0200

    add crawler testcase for wront filename case

--- exam-reminder-test/src/at/tugraz/examreminder/crawler/TuGrazSearchCrawlerTest.java
+++ exam-reminder-test/src/at/tugraz/examreminder/crawler/TuGrazSearchCrawlerTest.java
@@ -3,7 +3,9 @@ package at.tugraz.examreminder.crawler;
 import android.test.InstrumentationTestCase;
 import at.tugraz.examreminder.core.Course;
 import at.tugraz.examreminder.core.Exam;
+import junit.framework.Assert;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.SortedSet;
@@ -15,9 +17,32 @@ public class TuGrazSearchCrawlerTest extends InstrumentationTestCase {
         List<Course> courses;
         SortedSet<Exam> exams;
         courses = crawler.getCourseListFromFile(getInstrumentation().getContext().getAssets().open("searchterm_analysis.xml"));
+        exams = crawler.getExamsFromFile(getInstrumentation().getContext().getAssets().open("searchterm_analysis.xml"), courses.get(6));
+        assertEquals("Wrong number of courses found!", courses.size(), 130);
+        assertEquals("Wrong number of exams found!", exams.size(), 1);
         exams = crawler.getExamsFromFile(getInstrumentation().getContext().getAssets().open("searchterm_analysis.xml"), courses.get(0));
-        assertTrue("No valid data returned with this crawl!", courses.size() == 130);
-        assertTrue(exams.size() != 0);
+        assertEquals("Wrong number of exams found!", exams.size(), 0);
+    }
+
+
+    public void testSearchFileErrorHandling() throws IOException {
+        TuGrazSearchCrawler crawler = new TuGrazSearchCrawler();
+        List<Course> courses = null;
+        SortedSet<Exam> exams = null;
+        try{
+            courses = crawler.getCourseListFromFile(getInstrumentation().getContext().getAssets().open(""));
+            Assert.fail("No FileNotFoundException thrown!");
+        }
+        catch(FileNotFoundException e) {
+            assertEquals("Courses found, even when there was no valid data file!", courses == null ? 0 : courses.size(), 0);
+        }
+        try{
+            exams = crawler.getExamsFromFile(getInstrumentation().getContext().getAssets().open(""), null);
+            Assert.fail("No FileNotFoundException thrown!");
+        }
+        catch(FileNotFoundException e) {
+            assertEquals("Exams found, even when there was no valid data file!", exams == null ? 0 : exams.size(), 0);
+        }
     }
 
 }
