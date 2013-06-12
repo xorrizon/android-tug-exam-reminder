3e8dea196e381ab4a40f4a0e92cd6620ab5831d6
Daniel Pail <daniel.pail@student.tugraz.at>
2013-06-12 19:42:16 +0200

    Fixed DailyListener tests

--- exam-reminder-test/src/at/tugraz/examreminder/service/DailyListenerTest.java
+++ exam-reminder-test/src/at/tugraz/examreminder/service/DailyListenerTest.java
@@ -9,12 +9,13 @@ import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
 import android.test.InstrumentationTestCase;
 import at.tugraz.examreminder.R;
+import at.tugraz.examreminder.crawler.SimpleMockCrawler;
 import com.commonsware.cwac.wakeful.AlarmReceiver;
 
 public class DailyListenerTest extends InstrumentationTestCase {
 	private Context context;
 	private AlarmReceiver alarmReceiver;
-	private static final int SLEEP_TIME=100;
+	private static final int SLEEP_TIME=400;
 
 	@Override
 	protected void setUp() throws Exception {
@@ -22,6 +23,7 @@ public class DailyListenerTest extends InstrumentationTestCase {
 		context = getInstrumentation().getTargetContext();
 		alarmReceiver = new AlarmReceiver();
 		init();
+        UpdateService.setCrawlerToUse(SimpleMockCrawler.class);
 	}
 
 	@Override
