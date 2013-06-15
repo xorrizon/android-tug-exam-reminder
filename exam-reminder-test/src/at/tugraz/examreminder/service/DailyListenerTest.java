3d64d39dc18654419b0d9a18c62941818849febc
Daniel P <xorrizon@gmail.com>
2013-06-15 11:21:11 +0200

    Reset CrawlerToUser in tests

--- exam-reminder-test/src/at/tugraz/examreminder/service/DailyListenerTest.java
+++ exam-reminder-test/src/at/tugraz/examreminder/service/DailyListenerTest.java
@@ -29,6 +29,7 @@ public class DailyListenerTest extends InstrumentationTestCase {
 	@Override
 	protected void tearDown() throws Exception {
 		init();
+        UpdateService.setCrawlerToUse(null);
 		super.tearDown();
 	}
 
