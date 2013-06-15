cb8b22628769f9a86afce52f1bfa7ba7c5ffb7f7
Thomas Rosmarin <thomas.rosmarin@gmail.com>
2013-06-15 11:17:14 +0200

    add test for course list serializer

--- exam-reminder-test/src/at/tugraz/examreminder/service/CourseListSerializerTest.java
+++ exam-reminder-test/src/at/tugraz/examreminder/service/CourseListSerializerTest.java
@@ -1,11 +1,29 @@
 package at.tugraz.examreminder.service;
 
-/**
- * Created with IntelliJ IDEA.
- * User: thoros
- * Date: 01/06/13
- * Time: 18:23
- * To change this template use File | Settings | File Templates.
- */
-public class CourseListSerializerTest {
+
+import android.test.InstrumentationTestCase;
+import at.tugraz.examreminder.core.Course;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseListSerializerTest extends InstrumentationTestCase{
+
+    public void testFromJasonToListToJason() throws IOException {
+    List<Course> courses = new ArrayList<Course>();
+        CourseListSerializer courseListSerializer = new CourseListSerializer();
+        courses = courseListSerializer.loadCourseListFromFile(getInstrumentation().getContext().getAssets().open("saved_course_container_sample.json"));
+        assertEquals("Wrong number of courses serialized.", 2, courses.size());
+        assertEquals("Wrong number of exams serialized.", 3, courses.get(0).exams.size()+courses.get(1).exams.size());
+        assertEquals("Other coursename expected.", "Mathematik II, M", courses.get(0).name);
+        courseListSerializer.saveCourseListToFile(courses);
+        List<Course> courses_new = courseListSerializer.loadCourseListFromFile();
+        assertEquals("Deserialization error.", courses.size(), courses_new.size());
+        assertEquals("Deserialization error.", courses.get(0), courses.get(0));
+        courses.clear();
+        courses_new.clear();
+    }
 }
+
